869e446739a88dd81024fbad1e1cc4909c736a56
wolf2lyon <josesebastiancu@gmail.com>
2025-12-15 14:43:27 -0500

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://wolf2lyon.github.io/gestion-tickets/",
+  base: "/gestion-tickets/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
